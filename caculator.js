3491f77087062e9a25bc4e30d3c195fc42d90356
hamzatarek17 <166939951+hamzatarek17@users.noreply.github.com>
2024-06-18 12:45:16 +0300

    Add files via upload

---
+++ caculator.js
@@ -0,0 +1,19 @@
+const Display = document.getElementById("Display");
+
+function appendToDisplay(input){
+    Display.value += input;
+}
+
+function clearDisplay(){
+    Display.value = "";
+}
+
+function calculate(){
+    try{
+        Display.value = eval(Display.value);    
+    }
+    catch(error){
+        Display.value = "error";
+    }
+
+}
